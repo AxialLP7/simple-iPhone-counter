44d7eeaad536b275664531f1f2c074586e444ed5
AxialLP7 <axial.jc@gmail.com>
2015-04-01 13:18:13 -0700

    first upload

--- simpleCounter/ViewController.swift
+++ simpleCounter/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet var counterOutputLabel: UILabel!
+    
+    // counter to record the number of times we clicked the counter
+    var currentCount: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func addOneButton(sender: UIButton) {
+        
+        currentCount++
+        
+        counterOutputLabel.text = "The current count is \(currentCount)."
+        
+        counterOutputLabel.textColor = UIColor.redColor()
+        
+    }
 
+    @IBAction func addTwoButton(sender: UIButton) {
+        currentCount += 2
+        
+        counterOutputLabel.text = "The current count is \(currentCount)."
+        
+        counterOutputLabel.textColor = UIColor.redColor()
+    }
 
 }
 
