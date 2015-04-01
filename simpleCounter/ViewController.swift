3ef011053ae0f34f5ccd102760505a8300d38683
AxialLP7 <axial.jc@gmail.com>
2015-04-01 12:55:21 -0700

    Initial Commit

---
+++ simpleCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleCounter
+//
+//  Created by Jer Cherng Law on 4/1/15.
+//  Copyright (c) 2015 evehandful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
